c96af7aab266d179762ba0b91dc794010f54f1f0
robertriosm <87707371+robertriosm@users.noreply.github.com>
2023-05-29 02:58:41 -0600

    avances

--- neoreact/src/pages/RegisterPet.js
+++ neoreact/src/pages/RegisterPet.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function RegisterPet() {
   const [validated, setValidated] = useState(false);
@@ -20,65 +19,48 @@ function RegisterPet() {
 
   return (
     <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>Name of the pet</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Name"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>pet username</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="pet username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please choose a pet username.
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
-        />
+      <Form.Group as={Col} md="4" controlId="validationCustom01">
+        <Form.Label>Pet's Name</Form.Label>
+        <Form.Control required type="text" placeholder="Pet's Name" />
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
       </Form.Group>
-      <Button type="submit">Register</Button>
+      <br />
+
+      <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+        <Form.Label>Rescue date</Form.Label>
+        <InputGroup hasValidation>
+          <Form.Control
+            type="date"
+            placeholder="email"
+            aria-describedby="inputGroupPrepend"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please choose a valid date.
+          </Form.Control.Feedback>
+        </InputGroup>
+      </Form.Group>
+      <br />
+
+      <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+        <Form.Label>Vaccines</Form.Label>
+        <Form.Check label="Parvovirus 1" feedbackType="invalid" />
+        <Form.Check label="Parvovirus 2" feedbackType="invalid" />
+        <Form.Check label="Parvovirus 3" feedbackType="invalid" />
+        <Form.Check label="Tetravalente 1" feedbackType="invalid" />
+        <Form.Check label="Tetravalente 2" feedbackType="invalid" />
+        <Form.Check label="Rabia" feedbackType="invalid" />
+      </Form.Group>
+      <br />
+
+      <Form.Group>
+        <Form.Label>Dog's fertility</Form.Label>
+        <Form.Check label="This dog is neutered" feedbackType="invalid" />
+      </Form.Group>
+      <br />
+
+      <Button type="submit">Register Pet</Button>
+      <br />
     </Form>
   );
 }
